68e0385f69dd82982b075fd129ec315efc310272
Justin Marrington <justin@marrington.net>
2013-11-05 18:26:37 +1000

    Initial Commit

---
+++ SayDo/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SayDo
+//
+//  Created by Justin Marrington on 5/11/2013.
+//  Copyright (c) 2013 LovelyHead. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+  return NSApplicationMain(argc, argv);
+}
